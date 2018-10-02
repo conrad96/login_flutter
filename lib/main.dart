179bfa52bdf261feb6b7040eb2c947fb37d91834
conrad96 <conrad.mugisha@gmail.com>
2018-10-02 06:13:57 +0300

    UI design of login app

--- lib/main.dart
+++ lib/main.dart
@@ -1,94 +1,18 @@
 import "package:flutter/material.dart";
+import "package:login_flutter/src/login_layout.dart";
 
-void main(){
-  runApp(Scratch());
-}
-
-class Scratch extends StatefulWidget{
-  @override
-    State<StatefulWidget> createState() {
-      return ScratchState();
-    }
-}
-class ScratchState extends State<Scratch>{
-  final myKey = GlobalKey<FormState>();
-   String EmailStr="";
-   String PasswordStr ="";
-  Widget build(BuildContext context)
+void main() => runApp(Style());
+class Style extends StatelessWidget{
+  Widget build(context)
   {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Login System"),
-        ),
-        body: loginLayout(),
+      title: "Fancy",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+        fontFamily: 'Nunito',
       ),
-    );
-  }
-  Widget loginLayout(){
-
-    return Form(
-      key: myKey,
-      child: Container(
-        margin: EdgeInsets.all(15.0),
-        child: Column(
-          children: <Widget>[
-            email(),
-            password(), 
-            Container(margin: EdgeInsets.only(top: 10.0)),           
-            submitButton()
-            ]
-        )
-      )
-    );
-  }
-   email(){
-    return TextFormField(
-      keyboardType: TextInputType.emailAddress,
-      decoration: InputDecoration(
-        hintText: "john.doe@example.com",
-        labelText: "Email",        
-      ),
-      validator: (String arg){
-        if(!arg.contains('@'))
-        {
-          return "Email Invalid. please try again";
-        }
-      },
-      onSaved: (String arg){
-        EmailStr = arg;
-      },
-    );
-  }
-  password(){
-    return TextFormField(
-      decoration:  InputDecoration(
-        labelText: "Password",
-      ),
-      obscureText: true,
-      validator: (String arg){
-        if(arg.length < 5)
-        {
-          return "password must me more than 5 characters";
-        }
-      },
-      onSaved: (String arg){
-        PasswordStr = arg;
-      },
-    );
-  }
-  submitButton(){
-    return RaisedButton(
-      splashColor: Colors.teal,
-      elevation: 5.0,
-      child: Text("Login"),
-      onPressed: (){
-        if(myKey.currentState.validate())
-        {
-          myKey.currentState.save();
-          print("EMail: $EmailStr \n Password: $PasswordStr ");
-        }
-      },
+      home: LoginLayout(),
     );
   }
 }
---
+++ lib/src/login_layout.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+class LoginLayout extends StatefulWidget{
+_LoginLayoutState createState() => _LoginLayoutState();
+}
+
+class _LoginLayoutState extends State<LoginLayout>{
+  Widget build(context){
+    //custom logo
+    final logo = Hero(
+      tag: 'Hero',
+      child: CircleAvatar(
+        backgroundColor: Colors.lime,
+        radius: 50.0,
+        child: Image.asset('assets/images/logo_2.png'),
+      ),
+    );
+    final email = TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      autofocus: false,
+      initialValue: 'conrad.mugisha@gmail.com',
+      decoration: InputDecoration(
+        hintText: "Email",
+        labelText: "Email Address",
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30.0)
+        )
+      ),
+    );
+    final password = TextFormField(
+      decoration: InputDecoration(
+        labelText: "Password",
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30.0)
+        )
+      ),
+    );
+    final loginBtn = Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: Material(
+        borderRadius: BorderRadius.circular(30.0),
+        shadowColor: Colors.lightBlueAccent.shade100,
+        elevation: 5.0,
+        child: MaterialButton(
+          minWidth: 200.0,
+          height: 42.0,
+          onPressed: (){
+
+          },
+          color: Colors.lightBlueAccent,
+          child: Text("Login", style: TextStyle(color: Colors.white)),    
+        ),
+
+      )
+    );
+    final forgotLabel = FlatButton(
+      child: Text("Forgot password ?",
+        style: TextStyle(color: Colors.black45) 
+      ),
+      onPressed: (){},
+    );
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: ListView(
+          shrinkWrap: true,
+          padding: EdgeInsets.only(left:15.0,right:15.0),
+          children: <Widget>[
+            logo,
+            SizedBox(height: 35.0),
+            email,
+            SizedBox(height: 10.0),
+            password,
+            SizedBox(height: 10.0),
+            forgotLabel,
+            SizedBox(height: 10.0),
+            loginBtn
+          ]
+        )
+      )
+    );
+  }
+}
